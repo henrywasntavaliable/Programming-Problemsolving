031373a497e85150eb6bf59cb6c156b309164487
henrywasntavaliable <up2529235@myport.ac.uk>
2025-11-17 11:04:27 +0000

    Add media_loop function to print message multiple times

---
+++ problem7.py
@@ -0,0 +1,6 @@
+def media_loop():
+    print("Welcome to The Media Hub!")
+    times = int(input("How many times should we say 'Media is great!'? "))
+    
+    for i in range(times):
+        print("Media is great!")
