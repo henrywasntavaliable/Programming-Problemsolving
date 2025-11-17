e3b1849714eebfa9dec71af08c34954415b4a093
henrywasntavaliable <up2529235@myport.ac.uk>
2025-11-17 11:02:58 +0000

    Add welcome_message and media_hub_info functions

---
+++ problem2.py
@@ -0,0 +1,7 @@
+def welcome_message(): 
+    print("Welcome to the media hub!")
+    print("We have been serving enterainemnt enthusiasts since 1995.")
+
+def media_hub_info(): 
+    print("Location: 123 Entertainment Avenue, Mediaville.")
+    print("Speciality: Rare and vintage films, music and games.")
